8767e3573825e6f9df8acb17e9aedabf233d5640
Patrick Thomson <patrickt@github.com>
2025-02-25 11:02:29 -0500

    Add CV.

--- Main.hs
+++ Main.hs
@@ -23,6 +23,10 @@ main = hakyll do
 
   match "css/*" $ compile compressCssCompiler
 
+  match "cv.pdf" do
+    route idRoute
+    compile getResourceLBS
+
   create ["stylesheet.css"] do
     route idRoute
     compile do
